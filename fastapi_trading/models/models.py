3e58f7bc94f4db6c45faa889edf20b7c4148214f
web2cap <web2cap@gmail.com>
2024-05-11 12:34:12 +0200

    setup alembic

---
+++ fastapi_trading/models/models.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+
+from sqlalchemy import (
+    JSON,
+    TIMESTAMP,
+    Column,
+    ForeignKey,
+    Integer,
+    MetaData,
+    String,
+    Table,
+)
+
+metadata = MetaData()
+
+roles = Table(
+    "roles",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("name", String, nullable=False),
+    Column("permissions", JSON),
+)
+
+users = Table(
+    "users",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("email", String, nullable=False),
+    Column("username", String, nullable=False),
+    Column("password", String, nullable=False),
+    Column("registered_at", TIMESTAMP, default=datetime.now),
+    Column("role_id", Integer, ForeignKey("roles.id")),
+)
