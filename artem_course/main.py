b362b038997b02d95b415f826148fe1994876fe4
web2cap <web2cap@gmail.com>
2024-06-02 16:51:23 +0200

    args schema example

--- artem_course/main.py
+++ artem_course/main.py
@@ -1,7 +1,7 @@
 from datetime import date
 from typing import Optional
 
-from fastapi import FastAPI, Query
+from fastapi import Depends, FastAPI, Query
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -13,19 +13,29 @@ class SHotel(BaseModel):
     stars: int
 
 
+class HoteslSearchArgs:
+    def __init__(
+        self,
+        location: str,
+        date_from: date,
+        date_to: date,
+        stars: Optional[int] = Query(None, ge=1, le=5),
+        has_spa: Optional[bool] = None,
+    ) -> None:
+        self.location = location
+        self.date_from = date_from
+        self.date_to = date_to
+        self.stars = stars
+        self.has_spa = has_spa
+
+
 @app.get("/hotels")
-def get_hotels(
-    location: str,
-    date_from: date,
-    date_to: date,
-    stars: Optional[int] = Query(None, ge=1, le=5),
-    has_spa: Optional[bool] = None,
-) -> list[SHotel]:
+def get_hotels(search_args: HoteslSearchArgs = Depends()):  #  -> list[SHotel]:
     hotels = [
         {"address": "Adr", "name": "Test hotel", "stars": 5},
         {"address": "Adr", "name": "Test hotel", "stars": 5},
     ]
-    return hotels
+    return search_args
 
 
 class SBooking(BaseModel):
