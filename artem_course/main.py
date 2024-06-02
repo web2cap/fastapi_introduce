f1e9be226b9a97f56c3ef2cc67e9345cd205c895
web2cap <web2cap@gmail.com>
2024-06-02 16:39:41 +0200

    response validation example

--- artem_course/main.py
+++ artem_course/main.py
@@ -7,6 +7,12 @@ from pydantic import BaseModel
 app = FastAPI()
 
 
+class SHotel(BaseModel):
+    address: str
+    name: str
+    stars: int
+
+
 @app.get("/hotels")
 def get_hotels(
     location: str,
@@ -14,8 +20,12 @@ def get_hotels(
     date_to: date,
     stars: Optional[int] = Query(None, ge=1, le=5),
     has_spa: Optional[bool] = None,
-):
-    return date_from, date_to
+) -> list[SHotel]:
+    hotels = [
+        {"address": "Adr", "name": "Test hotel", "stars": 5},
+        {"address": "Adr", "name": "Test hotel", "stars": 5},
+    ]
+    return hotels
 
 
 class SBooking(BaseModel):
