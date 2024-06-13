0496455bac9dc838823d0f5a22b9bb346b4e9aa6
web2cap <web2cap@gmail.com>
2024-06-13 22:10:55 +0200

    bookings response validation

--- artem_course/app/bookings/router.py
+++ artem_course/app/bookings/router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
 from app.bookings.dao import BookingDAO
+from app.bookings.schemas import SBookings
 
 router = APIRouter(
     prefix="/bookings",
@@ -8,5 +9,6 @@ router = APIRouter(
 )
 
 @router.get("")
-async def get_bookings():
+async def get_bookings() -> list[SBookings]:
     return await BookingDAO.find_all()
+
