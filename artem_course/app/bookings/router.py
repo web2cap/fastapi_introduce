0a16c5ba6bdd5da9450ba56e5a54e146e6805a26
web2cap <web2cap@gmail.com>
2024-06-22 16:15:24 +0200

    find all bookings for user

--- artem_course/app/bookings/router.py
+++ artem_course/app/bookings/router.py
@@ -1,16 +1,20 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
 from app.bookings.dao import BookingDAO
 from app.bookings.schemas import SBookings
+from app.users.dependecies import get_current_user
+from app.users.models import Users
 
 router = APIRouter(
     prefix="/bookings",
     tags=["Bookings"],
 )
 
+
 @router.get("")
-async def get_bookings() -> list[SBookings]:
-    return await BookingDAO.find_all()
+async def get_bookings(user: Users = Depends(get_current_user)) -> list[SBookings]:
+    return await BookingDAO.find_all(user_id=user.id)
+
 
 @router.get("/{id}")
 async def get_bookings_by_id(id: int) -> SBookings | None:
