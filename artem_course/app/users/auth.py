fd48492b4d01e526ad279c521d10bb4744e18e08
web2cap <web2cap@gmail.com>
2024-06-22 14:46:27 +0200

    auth login utils

--- artem_course/app/users/auth.py
+++ artem_course/app/users/auth.py
@@ -1,4 +1,11 @@
+from datetime import datetime, timedelta, timezone
+
 import bcrypt
+from jose import jwt
+from pydantic import EmailStr
+
+from app.config import settings
+from app.users.dao import UsersDAO
 
 
 def hash_password(password: str) -> str:
@@ -14,3 +21,17 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
     return bcrypt.checkpw(
         password=plain_password_byte_enc, hashed_password=hashed_password_byte_enc
     )
+
+
+async def authenticate_user(email: EmailStr, password: str) -> UsersDAO | None:
+    user = await UsersDAO.find_one_or_none(email=email)
+    if not user or not verify_password(password, user.hashed_password):
+        return None
+    return user
+
+
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(days=5)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, settings.HASH_KEY, settings.HASH_ALGO)
