acaaf0ea135c31d37e6f5d48962c0f3f7e68035b
web2cap <web2cap@gmail.com>
2024-06-22 14:46:43 +0200

    login router

--- artem_course/app/users/router.py
+++ artem_course/app/users/router.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Response, status
 
-from app.users.auth import hash_password
+from app.users.auth import authenticate_user, create_access_token, hash_password
 from app.users.dao import UsersDAO
-from app.users.schemas import SUserRegister
+from app.users.schemas import SUserAuth
 
 router = APIRouter(
     prefix="/auth",
@@ -11,12 +11,29 @@ router = APIRouter(
 
 
 @router.post("/register")
-async def register_user(user_data: SUserRegister) -> dict:
+async def register_user(user_data: SUserAuth) -> dict:
     existing_users = await UsersDAO.find_one_or_none(email=user_data.email)
     if existing_users:
-        raise HTTPException(status_code=409, detail="This email is already registered")
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="This email is already registered",
+        )
 
     hashed_password = hash_password(user_data.password)
     await UsersDAO.insetr_data(email=user_data.email, hashed_password=hashed_password)
 
     return {"status": "created"}
+
+
+@router.post("/login")
+async def login_user(response: Response, user_data: SUserAuth) -> dict:
+    user = await authenticate_user(user_data.email, user_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Wrong email or password"
+        )
+
+    access_token = create_access_token({"sub": user.id})
+    response.set_cookie("booking_access_token", access_token, httponly=True)
+
+    return {"id": user.id, "email": user.email}
