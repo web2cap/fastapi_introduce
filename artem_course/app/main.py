2ede52f9addf2f38ead7d48ca35b11d794363ae3
web2cap <web2cap@gmail.com>
2024-06-29 10:27:43 +0200

    jinja template for hotels

--- artem_course/app/main.py
+++ artem_course/app/main.py
@@ -5,12 +5,14 @@ from fastapi import Depends, FastAPI, Query
 from pydantic import BaseModel
 
 from app.bookings.router import router as router_bookings
+from app.pages.router import router as router_pages
 from app.users.router import router as router_users
 
 app = FastAPI()
 
 app.include_router(router_users)
 app.include_router(router_bookings)
+app.include_router(router_pages)
 
 
 class SHotel(BaseModel):
@@ -19,29 +21,29 @@ class SHotel(BaseModel):
     stars: int
 
 
-class HoteslSearchArgs:
-    def __init__(
-        self,
-        location: str,
-        date_from: date,
-        date_to: date,
-        stars: Optional[int] = Query(None, ge=1, le=5),
-        has_spa: Optional[bool] = None,
-    ) -> None:
-        self.location = location
-        self.date_from = date_from
-        self.date_to = date_to
-        self.stars = stars
-        self.has_spa = has_spa
-
-
-@app.get("/hotels")
-def get_hotels(search_args: HoteslSearchArgs = Depends()):  #  -> list[SHotel]:
-    hotels = [
-        {"address": "Adr", "name": "Test hotel", "stars": 5},
-        {"address": "Adr", "name": "Test hotel", "stars": 5},
-    ]
-    return search_args
+# class HoteslSearchArgs:
+#     def __init__(
+#         self,
+#         location: str,
+#         date_from: date,
+#         date_to: date,
+#         stars: Optional[int] = Query(None, ge=1, le=5),
+#         has_spa: Optional[bool] = None,
+#     ) -> None:
+#         self.location = location
+#         self.date_from = date_from
+#         self.date_to = date_to
+#         self.stars = stars
+#         self.has_spa = has_spa
+#
+#
+# @app.get("/hotels")
+# def get_hotels(search_args: HoteslSearchArgs = Depends()):  #  -> list[SHotel]:
+#     hotels = [
+#         {"address": "Adr", "name": "Test hotel", "stars": 5},
+#         {"address": "Adr", "name": "Test hotel", "stars": 5},
+#     ]
+#     return search_args
 
 
 class SBooking(BaseModel):
