d3325d50a60439f0ec3b7b6e62b0f78be5102daa
web2cap <web2cap@gmail.com>
2024-07-15 17:56:47 +0200

    simple hotels print

--- artem_course/app/pages/router.py
+++ artem_course/app/pages/router.py
@@ -1,5 +1,7 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Depends, Request
 from fastapi.templating import Jinja2Templates
+
+from app.hotels.router import get_hotels_by_location
 
 router = APIRouter(prefix="/pages", tags=["Frontend"])
 
@@ -7,5 +9,7 @@ templates = Jinja2Templates(directory="app/templates")
 
 
 @router.get("/hotels")
-async def get_hotels_page(request: Request):
-    return templates.TemplateResponse(name="hotels.html", context={"request": request})
+async def get_hotels_page(request: Request, hotels=Depends(get_hotels_by_location)):
+    return templates.TemplateResponse(
+        name="hotels.html", context={"request": request, "hotels": hotels}
+    )
