770a8144b1816e3217bd89c6764e5dc6493b08f9
web2cap <web2cap@gmail.com>
2024-07-22 13:12:51 +0200

    hotels rooms relationship

--- artem_course/app/hotels/rooms/models.py
+++ artem_course/app/hotels/rooms/models.py
@@ -1,14 +1,22 @@
-from sqlalchemy import JSON, Column, Integer, String, ForeignKey
+from sqlalchemy import JSON, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
 from app.database import Base
+
 
 class Rooms(Base):
     __tablename__ = "rooms"
 
     id = Column(Integer, primary_key=True)
     hotel_id = Column(ForeignKey("hotels.id"))
-    name  = Column(String, nullable=False)
+    name = Column(String, nullable=False)
     description = Column(String)
     price = Column(Integer, nullable=False)
     services = Column(JSON)
     quantity = Column(Integer, nullable=False)
     image_id = Column(Integer)
+
+    hotels = relationship("Hotels", back_populates="rooms")
+
+    def __str__(self):
+        return self.name
