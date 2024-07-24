5dbb27abff77d33890f5b0d94c9c1d274a7f0709
web2cap <web2cap@gmail.com>
2024-07-24 14:03:15 +0200

    fix DATABASE_PARAMS for TEST MODE

--- artem_course/app/database.py
+++ artem_course/app/database.py
@@ -1,15 +1,15 @@
-from sqlalchemy import NullPool
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from sqlalchemy.pool import NullPool
 
 from app.config import settings
 
 if settings.MODE == "TEST":
     DATABASE_URL = f"postgresql+asyncpg://{settings.TEST_DB_USER}:{settings.TEST_DB_PASS}@{settings.TEST_DB_HOST}:{settings.TEST_DB_PORT}/{settings.TEST_DB_NAME}"
-    DATABASE_PARAMS = {}
+    DATABASE_PARAMS = {"poolclass": NullPool}
 else:
     DATABASE_URL = f"postgresql+asyncpg://{settings.DB_USER}:{settings.DB_PASS}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
-    DATABASE_PARAMS = {"poolclass": NullPool}
+    DATABASE_PARAMS = {}
 
 
 engine = create_async_engine(DATABASE_URL, **DATABASE_PARAMS)
