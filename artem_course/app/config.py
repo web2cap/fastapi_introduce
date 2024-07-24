24618f57af7d9d78d12a71aa3358c39326cdcb32
web2cap <web2cap@gmail.com>
2024-07-24 10:37:06 +0200

    modes for connect to DBs

--- artem_course/app/config.py
+++ artem_course/app/config.py
@@ -1,12 +1,22 @@
+from typing import Literal
+
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
+    MODE: Literal["DEV", "TEST", "PROD"]
+
     DB_HOST: str
     DB_PORT: int
     DB_NAME: str
     DB_USER: str
     DB_PASS: str
+
+    TEST_DB_HOST: str
+    TEST_DB_PORT: int
+    TEST_DB_NAME: str
+    TEST_DB_USER: str
+    TEST_DB_PASS: str
 
     REDIS_CACHE_STR: str
     REDIS_CELERY_STR: str
