392412bae46e3fb8aae678a0703b8e5a7fadbf27
web2cap <web2cap@gmail.com>
2024-07-26 16:14:22 +0200

    add logder

--- artem_course/app/main.py
+++ artem_course/app/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+import time
+
+from fastapi import FastAPI, Request
 from fastapi.concurrency import asynccontextmanager
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
@@ -15,13 +17,16 @@ from app.database import engine
 from app.hotels.rooms.router import router as router_rooms
 from app.hotels.router import router as router_hotels
 from app.images.router import router as router_images
+from app.logger import logger
 from app.pages.router import router as router_pages
 from app.users.router import router as router_users
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    redis = aioredis.from_url(settings.REDIS_CACHE_STR, encoding="utf8", decode_responses=True)
+    redis = aioredis.from_url(
+        settings.REDIS_CACHE_STR, encoding="utf8", decode_responses=True
+    )
     FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
     yield
     await redis.close()
@@ -54,8 +59,18 @@ app.add_middleware(
 )
 
 
-admin = Admin(app, engine, authentication_backend=authentication_backend)
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    logger.info(
+        "Request execution time", extra={"process_time": round(process_time, 4)}
+    )
+    return response
 
+
+admin = Admin(app, engine, authentication_backend=authentication_backend)
 
 admin.add_view(UserAdmin)
 admin.add_view(BookingsAdmin)
