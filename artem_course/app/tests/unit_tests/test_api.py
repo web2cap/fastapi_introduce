6f7e71b26372ae43b48e9cd5a4f0bcc8ba322b07
web2cap <web2cap@gmail.com>
2024-07-26 13:07:47 +0200

    login test

--- artem_course/app/tests/unit_tests/test_api.py
+++ artem_course/app/tests/unit_tests/test_api.py
@@ -2,10 +2,41 @@ import pytest
 from httpx import AsyncClient
 
 
+@pytest.mark.parametrize(
+    "email, password, status_code",
+    [
+        ("kot@pes.com", "kotopes", 201),
+        ("kot@pes.com", "kotOpes", 409),
+        ("noemail", "kotOpes", 422),
+        ("", "kotOpes", 422),
+        (None, "kotOpes", 422),
+        ("pes@pes.com", None, 422),
+    ],
+)
 @pytest.mark.asyncio
-async def test_register_user(ac: AsyncClient):
+async def test_register_user(email, password, status_code, ac: AsyncClient):
     response = await ac.post(
         "/auth/register",
-        json={"email": "new@user.com", "password": "Passwd"},
+        json={"email": email, "password": password},
     )
-    assert response.status_code == 201
+    assert response.status_code == status_code
+
+
+@pytest.mark.parametrize(
+    "email, password, status_code",
+    [
+        ("test@test.com", "test", 200),
+        ("artem@example.com", "artem", 200),
+        ("doestnexist@example.com", "artem", 401),
+    ],
+)
+@pytest.mark.asyncio
+async def test_login_user(email, password, status_code, ac: AsyncClient):
+    response = await ac.post(
+        "/auth/login",
+        json={
+            "email": email,
+            "password": password,
+        },
+    )
+    assert response.status_code == status_code
