8310da4232800b13e28c658d8bee524df486032b
web2cap <web2cap@gmail.com>
2024-07-26 16:14:08 +0200

    fix psql string

--- artem_course/app/database.py
+++ artem_course/app/database.py
@@ -7,13 +7,13 @@ from app.config import settings
 if settings.MODE == "TEST":
     DATABASE_URL = (
         f"postgresql+asyncpg://{settings.TEST_DB_USER}:{settings.TEST_DB_PASS}@"
-        "{settings.TEST_DB_HOST}:{settings.TEST_DB_PORT}/{settings.TEST_DB_NAME}"
+        f"{settings.TEST_DB_HOST}:{settings.TEST_DB_PORT}/{settings.TEST_DB_NAME}"
     )
     DATABASE_PARAMS = {"poolclass": NullPool}
 else:
     DATABASE_URL = (
         f"postgresql+asyncpg://{settings.DB_USER}:{settings.DB_PASS}@"
-        "{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
+        f"{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
     )
     DATABASE_PARAMS = {}
 
