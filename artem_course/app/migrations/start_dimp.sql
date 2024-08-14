652472dbc93942da23e60b604c2e33d8bf261c53
web2cap <web2cap@gmail.com>
2024-08-14 21:04:49 +0200

    dump for new users model

--- artem_course/app/migrations/start_dimp.sql
+++ artem_course/app/migrations/start_dimp.sql
@@ -20,9 +20,10 @@ INSERT INTO rooms (hotel_id, name, description, price, quantity, services, image
 (5, 'Номер полулюкс премиум (с завтраком)', 'Полулюкс - он и есть полулюкс, шик и блеск.', 8000, 7, '[]', 16),
 (6, 'Стандарт (типовой корпус)', 'Стандартный номер.', 8125, 45, '[]', 17);
 
-INSERT INTO users (email, hashed_password) VALUES 
-('fedor@moloko.ru', 'tut_budet_hashed_password_1'),
-('sharik@moloko.ru', 'tut_budet_hashed_password_2');
+INSERT INTO "user" (email, hashed_password, is_active, is_superuser, is_verified) VALUES 
+('user@example.com', '$argon2id$v=19$m=65536,t=3,p=4$CdA+1uNurh7cIeWF95zFgw$/XMlvwHaFYFfY109g+YGbXIc7tUrb1gtOWfSyIEkGrU', true, false, false),
+('user1@example.com', '$argon2id$v=19$m=65536,t=3,p=4$W7y7u2ysMmQeSLO6PndkAg$pCr2roDHNlrqN/7ovMV8VMBM10ifnV+jeXWZliaftXw', true, false, false),
+('admin@example.com', '$argon2id$v=19$m=65536,t=3,p=4$rBD+n6YzxCjKCahKlf3mcw$p+/RLYc6Fwe4XrOFgmhOnoyflQSLe0Rugu3JJosMxoQ', true, true, true);
 
 INSERT INTO bookings (room_id, user_id, date_from, date_to, price) VALUES
 (1, 1, '2023-06-15', '2023-06-30', 24500),
