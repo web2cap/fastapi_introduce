ade82d4152d368ff6129d9a2868c9fd803a9e866
web2cap <web2cap@gmail.com>
2024-08-14 19:25:38 +0200

    get_async_session

--- artem_course/app/users/user_db.py
+++ artem_course/app/users/user_db.py
@@ -1,9 +1,17 @@
+from typing import AsyncGenerator
+
+from fastapi import Depends
 from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.database import async_session_maker
 from app.users.models import Users
 
 
-async def get_user_db():
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session_maker() as session:
-        yield SQLAlchemyUserDatabase(session, Users)
+        yield session
+
+
+async def get_user_db(session: AsyncSession = Depends(get_async_session)):
+    yield SQLAlchemyUserDatabase(session, Users)
